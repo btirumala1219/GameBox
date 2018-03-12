473812c6319cdcddeda0114ab9ac7ab9d26ffcd6
btirumala <barath.tirumala@gmail.com>
2018-03-12 19:59:08 -0400

    Added hanging man functionality, included correct/wrong label

--- Script/hangman.js
+++ Script/hangman.js
@@ -2,57 +2,61 @@
 //var fileName = "../Content/data.txt";
 var word = 'BISHOP';
 var wordarray = word.split("");
+var cnt = 1;
 
 for(var k = 0; k<6; k++){
   var idnum = k+1;
   var id = 'l'+idnum;
-  console.log(id);
+  //console.log(id);
   document.getElementById(id).style.visibility = "hidden";
-}
-
-function createSprite(element, x, y, w, h) {
-  var result = new Object();
-  result.element = element;
-  result.x = x;
-  result.y = y;
-  result.w = w;
-  result.h = h;
-  return result;
-}
-
-function setPosition(sprite) {
-  var e = document.getElementById(sprite.element);
-  e.style.left = sprite.x + 'px';
-  e.style.top = sprite.y + 'px';
-}
-
-function showSprites() {
-  setPosition(hero);
-  setPosition(laser);
-  for (var i = 0; i < enemies.length; i++) {
-    setPosition(enemies[i]);
-  }
+  var bid = 'b'+idnum;
+  console.log(bid);
+  document.getElementById(bid).style.visibility = "hidden";
+  document.getElementById('correct').style.visibility = "hidden";
+  document.getElementById('wrong').style.visibility = "hidden";
 }
 
 function gotWordRight(index){
-  showLetter(index);
   console.log("CORRECT");
   var idnum = index+1;
   var id = 'l'+idnum;
+  console.log(id);
   document.getElementById(id).style.visibility = "visible";
+  var bid = 'b' + idnum;
+  document.getElementById(id).style.visibility = "visible";
+  document.getElementById('correct').style.visibility = "visible";
+  document.getElementById('wrong').style.visibility = "hidden";
+  //cnt++
 }
 
-function showLetter(index) {
+function gotWordWrong(){
+  console.log("WRONG");
+  var bid = 'b' + cnt;
+  document.getElementById(bid).style.visibility = "visible";
+  console.log(bid);
+  cnt++;
+  document.getElementById('wrong').style.visibility = "visible";
+  document.getElementById('correct').style.visibility = "hidden";
+}
+
+function checkTruth(tempo) {
 
 }
 
 function toggleKey(keyCode) {
   var tempo = String.fromCharCode(keyCode);
   //console.log(tempo);
+  var check = 0;
   for(var i = 0; i< word.length; i++){
     if(wordarray[i] == tempo){
       gotWordRight(i);
     }
+    else{
+      check++;
+    }
+  }
+  if(check != 5){
+    gotWordWrong();
   }
 }
 
