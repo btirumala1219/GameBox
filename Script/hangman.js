615079771bfe6d2a345753d6ba189a4708fe4e97
btirumala <barath.tirumala@gmail.com>
2018-03-13 00:15:20 -0400

    Work for implementing random word

--- Script/hangman.js
+++ Script/hangman.js
@@ -1,66 +1,89 @@
 
 //var fileName = "../Content/data.txt";
-var word = 'BISHOP';
-var wordarray = word.split("");
-var cnt = 1;
 
-for(var k = 0; k<6; k++){
-  var idnum = k+1;
-  var id = 'l'+idnum;
-  //console.log(id);
-  document.getElementById(id).style.visibility = "hidden";
-  var bid = 'b'+idnum;
-  console.log(bid);
-  document.getElementById(bid).style.visibility = "hidden";
-  document.getElementById('correct').style.visibility = "hidden";
-  document.getElementById('wrong').style.visibility = "hidden";
-}
+init();
 
-function gotWordRight(index){
-  console.log("CORRECT");
-  var idnum = index+1;
-  var id = 'l'+idnum;
-  console.log(id);
-  document.getElementById(id).style.visibility = "visible";
-  var bid = 'b' + idnum;
-  document.getElementById(id).style.visibility = "visible";
-  document.getElementById('correct').style.visibility = "visible";
-  document.getElementById('wrong').style.visibility = "hidden";
-  //cnt++
-}
+function init(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+    if (this.readyState == 4 && this.status == 200){
+      var lines = this.responseText.split("\n");
+      var rand = Math.floor(Math.random() * 25);
+      var word = lines[rand];
+      document.getElementById("tt").innerHTML = word;
+      console.log(document.getElementById("tt").textContent);
+      //var word = 'BISHOP';
+      console.log(document.getElementById("tt").textContent);
+      //console.log(word);
+      var wordarray = word.split("");
+      var cnt = 1;
 
-function gotWordWrong(){
-  console.log("WRONG");
-  var bid = 'b' + cnt;
-  document.getElementById(bid).style.visibility = "visible";
-  console.log(bid);
-  cnt++;
-  document.getElementById('wrong').style.visibility = "visible";
-  document.getElementById('correct').style.visibility = "hidden";
-}
+      for(var k = 0; k<6; k++){
+        var idnum = k+1;
+        var id = 'l'+idnum;
+        //console.log(id);
+        document.getElementById(id).style.visibility = "hidden";
+        var bid = 'b'+idnum;
+        //console.log(bid);
+        document.getElementById(bid).style.visibility = "hidden";
+        document.getElementById('correct').style.visibility = "hidden";
+        document.getElementById('wrong').style.visibility = "hidden";
+      }
 
-function checkTruth(tempo) {
+      function gotWordRight(index){
+        console.log("CORRECT");
+        var idnum = index+1;
+        var id = 'l'+idnum;
+        console.log(id);
+        document.getElementById(id).style.visibility = "visible";
+        var bid = 'b' + idnum;
+        document.getElementById(id).style.visibility = "visible";
+        document.getElementById('correct').style.visibility = "visible";
+        document.getElementById('wrong').style.visibility = "hidden";
+        //cnt++
+      }
 
-}
+      function gotWordWrong(){
+        console.log("WRONG");
+        var bid = 'b' + cnt;
+        document.getElementById(bid).style.visibility = "visible";
+        console.log(bid);
+        cnt++;
+        document.getElementById('wrong').style.visibility = "visible";
+        document.getElementById('correct').style.visibility = "hidden";
+      }
 
-function toggleKey(keyCode) {
-  var tempo = String.fromCharCode(keyCode);
-  //console.log(tempo);
-  var check = 0;
-  for(var i = 0; i< word.length; i++){
-    if(wordarray[i] == tempo){
-      gotWordRight(i);
+      function checkTruth(tempo) {
+
+      }
+
+      function toggleKey(keyCode) {
+        var tempo = String.fromCharCode(keyCode);
+        //console.log(tempo);
+        var check = 0;
+        for(var i = 0; i< word.length; i++){
+          if(wordarray[i] == tempo){
+            gotWordRight(i);
+          }
+          else{
+            check++;
+          }
+        }
+        if(check != 5){
+          gotWordWrong();
+        }
+      }
+
+      document.onkeydown = function(evt) {
+        toggleKey(evt.keyCode);
+      };
+
     }
-    else{
-      check++;
-    }
-  }
-  if(check != 5){
-    gotWordWrong();
-  }
+  };
+  xhttp.open("GET", "../Content/data.txt", true);
+  xhttp.send();
 }
 
-document.onkeydown = function(evt) {
-  toggleKey(evt.keyCode);
 
-};
+
+//localStorage.setItem("word", 'HHHH');
